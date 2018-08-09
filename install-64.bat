93583ed9b69d075fea20e921daa62702cb1f97a8
Rodrigo Jose Weffer Cifuentes <rodrigo.jose.weffer.cifuentes@zalando.de>
2018-08-09 22:17:29 +0200

    Only the windows installer things

--- install-64.bat
+++ install-64.bat
@@ -1,5 +1,4 @@
 start install\python-3.6.1-amd64-webinstall.exe
-start install\sqlite-netFx20-setup-bundle-x64-2005-1.0.105.1.exe
 start install\dotNetFx35setup.exe
 python install\get-pip.py
 pip install -r requests.txt
